63aafbf51bad3eca20ef5cc5f4b831492f6e3c6c
Valentina-K <kvalentyna@hotmail.com>
2022-11-29 21:06:40 +0200

    Initial player and install lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+console.log(Vimeo);
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+let currentTime;
+const player = new Vimeo(iframe);
+    const onTimeUpdate = function(data) {
+    // data is an object containing properties specific to that event
+};
 
+player.on('timeupdate', onTimeUpdate);
+
+    
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
