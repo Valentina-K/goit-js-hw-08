d6ead3dc15887ef736d9fce133b6a92f8993b5ba
Valentina-K <kvalentyna@hotmail.com>
2022-12-01 18:08:11 +0200

    task-02 is ok

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
 import Vimeo from '@vimeo/player';
-console.log(Vimeo);
+import throttle from 'lodash.throttle';
+
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
 const iframe = document.querySelector('#vimeo-player');
-console.log(iframe);
-let currentTime;
 const player = new Vimeo(iframe);
-    const onTimeUpdate = function(data) {
-    // data is an object containing properties specific to that event
+
+player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY) || 0).then(function (seconds) {    
+}).catch(function (error) { console.log(error.message) });
+
+const onTimeUpdate = function (data) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);   
 };
+player.on('timeupdate', throttle(onTimeUpdate,1000));
 
-player.on('timeupdate', onTimeUpdate);
 
-    
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
